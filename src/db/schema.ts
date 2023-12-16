638b1695b9a3949a69ea1611573b8b98a7126671
Kirumi1019 <wobd124d@gmail.com>
2023-12-16 14:48:18 +0800

    Order table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { char, integer, primaryKey, index, pgTable,unique, varchar, serial } from "drizzle-orm/pg-core";
+import { char, integer, primaryKey, index, pgTable,unique, varchar, serial, uuid, timestamp,} from "drizzle-orm/pg-core";
 
 
 export const membersTable = pgTable(
@@ -39,4 +39,61 @@ export const memberRoleTable = pgTable(
       }),
 );
 
+export const ordersTable = pgTable(
+  "orders",
+  {
+    orderId: uuid("Order_id").defaultRandom().notNull().unique().primaryKey(),
+    // displayId: uuid("display_id").defaultRandom().notNull().unique(),
+    buyerId: char("Buyer_id", {length: 9}).notNull().references(()=>membersTable.schoolID, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    transactionStatus: varchar("Transaction status", {length: 8,  enum: ["Complete","Cancel","Underway"]}).notNull(),
+    buyerConfirmation: varchar("Buyer_confirmation ", {length: 10,  enum: ["Unfinished","Finished"]}).notNull().default("Unfinished"),
+    sellerConfirmation: varchar("Seller_confirmation ", {length: 10,  enum: ["Unfinished","Finished"]}).notNull().default("Unfinished"),
+    transactionRate: integer("Transaction_rate ").notNull(),
+    transactionContent: varchar("Transaction_content ", {length: 50}),
+    placeOrderDatetime: timestamp("timestamp").defaultNow().notNull(),
 
+  },
+  (table) => ({
+    
+  }),
+);
+
+export const ordersContainTable = pgTable(
+  "orders_contain",
+  {
+    orderId: uuid("Order_id").defaultRandom().notNull().references(()=>ordersTable.orderId, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    productId: uuid("Product_id").defaultRandom().notNull().references(()=>productTable.productID, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    // displayId: uuid("display_id").defaultRandom().notNull().unique(),
+    quantity: integer("Quantity").notNull(),
+
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.orderId, table.productId] }),
+  }),
+);
+
+export const deliveryTable = pgTable(
+  "delivery",
+  {
+    orderId: uuid("Order_id").defaultRandom().notNull().references(()=>ordersTable.orderId, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }), //訂單編號
+    // displayId: uuid("display_id").defaultRandom().notNull().unique(),
+    deliveryMethod: varchar("Delivery_method", { length: 10, enum:["Freight","FaceToFace"] }).notNull(),
+    doneDeliveryDatetime: timestamp("Done_delivery_datetime").defaultNow(),
+    doneDliveryLocation: varchar("Done_delivery_location", { length: 50 }),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.orderId, table.deliveryMethod] }),
+  }),
+);
